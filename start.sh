44368f8661c7bf3466fb7b40d751204133663733
Joaquim Campelo da Silva Neto <joaquim.neto@ati.to.gov.br>
2023-05-09 10:25:24 -0300

    🚧 Criando view Home

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 source docker-compose/funcoes.sh
 docker-compose up -d --build
+
+# empacotando o projeto
+app mvn clean package
+
+# executando jar
+app java -jar target/projeto-teste-0.0.1-SNAPSHOT.jar
