47698619c46ea22291f190e8c00e926df5bfb377
Joaquim Campelo da Silva Neto <joaquim.neto@ati.to.gov.br>
2023-05-12 18:10:10 -0300

    🚧 Adiciona classe Person

--- demo/src/main/java/com/example/demo/controllers/PersonController.java
+++ demo/src/main/java/com/example/demo/controllers/PersonController.java
@@ -1,20 +1,29 @@
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.controllers.Person;
+
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class PersonController {
 
     private final JdbcTemplate jdbcTemplate;
 
+    @Autowired
     public PersonController(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    @GetMapping("/api/people")
-    public List<Map<String, Object>> getPeople() {
+    @GetMapping("/people")
+    public List<Person> getPeople() {
         String sql = "SELECT name, age FROM people";
-        return jdbcTemplate.queryForList(sql);
+        return jdbcTemplate.query(sql, (rs, rowNum) ->
+                new Person(rs.getString("name"), rs.getInt("age")));
     }
 
 }
