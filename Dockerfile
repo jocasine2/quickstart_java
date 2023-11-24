4ac0a7775603ed801ccc28c6cbc7c33ca4128a11
Joaquim Campelo da Silva Neto <joaquim.neto@ati.to.gov.br>
2023-11-24 20:37:13 -0300

    simula acesso humano a site com selenium

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,46 @@ FROM openjdk:17-jdk-alpine
 # Instala o Maven
 RUN apk add --no-cache bash maven
 
+RUN apk update && apk add --no-cache bash \
+        alsa-lib \
+        at-spi2-atk \
+        atk \
+        cairo \
+        cups-libs \
+        dbus-libs \
+        eudev-libs \
+        expat \
+        flac \
+        gdk-pixbuf \
+        glib \
+        libgcc \
+        libjpeg-turbo \
+        libpng \
+        libwebp \
+        libx11 \
+        libxcomposite \
+        libxdamage \
+        libxext \
+        libxfixes \
+        tzdata \
+        libexif \
+        udev \
+        xvfb \
+        zlib-dev \
+        chromium \
+        chromium-chromedriver
+
 # Define um diretório de trabalho no contêiner
 WORKDIR /app
 
 # Copie os arquivos do aplicativo e o arquivo .env para o diretório de trabalho
 COPY ./app .
+
+# Criar um usuário não root
+RUN adduser -D -u 1001 joaquim
+
+# Define o usuário não root para execução do aplicativo
+# USER joaquim
 
 # Expõe a porta 8080 para acesso externo (se necessário)
 EXPOSE 8080
--- app/src/main/java/com/example/demo/controllers/TestController.java
+++ app/src/main/java/com/example/demo/controllers/TestController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controllers;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +14,33 @@ public class TestController {
 
     @RequestMapping("/primeiro-projeto")
     public String primeiroProjeto() {
-        return "Teste com sucesso!";
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless", "--disable-gpu", "--remote-debugging-port=9222", "--no-sandbox");
+
+        try {
+            WebDriver driver = new ChromeDriver(chromeOptions);
+
+            // Navega até a página do Google
+            driver.get("https://idp.transferegov.sistema.gov.br/idp/");
+
+            // Aguarda alguns segundos para garantir que a página foi totalmente carregada
+            Thread.sleep(5000);
+
+            // Obtém o conteúdo da página
+            String pageContent = driver.getPageSource();
+
+            // Exibe o conteúdo da página
+            System.out.println(pageContent);
+
+            // Certifique-se de fechar o driver após o uso
+            driver.quit();
+
+            return pageContent;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Erro ao executar o teste: " + e.getMessage();
+        }
     }
 }
