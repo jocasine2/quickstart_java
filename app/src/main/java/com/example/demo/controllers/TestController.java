b8afc5cd8ca50278c9ec2916a94ab015c29f4f53
Joaquim Campelo da Silva Neto <joaquim.neto@ati.to.gov.br>
2023-11-25 02:11:07 -0300

    ✨ Navega até a pagina que contém a lista de codigos de programas e adiciona-os a um array

--- app/src/main/java/com/example/demo/controllers/TestController.java
+++ app/src/main/java/com/example/demo/controllers/TestController.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class TestController {
@@ -22,22 +25,31 @@ public class TestController {
         try {
             WebDriver driver = new ChromeDriver(chromeOptions);
 
-            // Navega até a página do Google
+            // Navega até a página de login
             driver.get("https://idp.transferegov.sistema.gov.br/idp/");
+            driver.findElement(By.linkText("Acesso livre")).click();
+            driver.findElement(By.cssSelector(".col1 > .button:nth-child(2)")).click();
+            driver.findElement(By.linkText("Consultar Programas")).click();
+            driver.findElement(By.id("consultarProgramaAtende")).click();
+            driver.findElement(By.xpath("(//input[@id='consultarProgramaAtende'])[2]")).click();
+            driver.findElement(By.xpath("(//input[@id='form_submit'])[3]")).click();
+            driver.findElement(By.id("ConteudoDiv")).click();
 
             // Aguarda alguns segundos para garantir que a página foi totalmente carregada
-            Thread.sleep(5000);
+            // Thread.sleep(5000);
 
-            // Obtém o conteúdo da página
-            String pageContent = driver.getPageSource();
-
-            // Exibe o conteúdo da página
-            System.out.println(pageContent);
+            // Coleta os códigos da tabela
+            List<String> codigos = new ArrayList<>();
+            List<WebElement> codigoElements = driver.findElements(By.cssSelector("#tbodyrow a[href*='Detalhar']"));
+            for (WebElement codigoElement : codigoElements) {
+                codigos.add(codigoElement.getText());
+            }
 
             // Certifique-se de fechar o driver após o uso
             driver.quit();
 
-            return pageContent;
+            // Retorna os códigos como uma string
+            return String.join("<br>", codigos);
         } catch (Exception e) {
             e.printStackTrace();
             return "Erro ao executar o teste: " + e.getMessage();
